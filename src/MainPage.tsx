8c73a4db6ddacd3697155e1881fb5e01f637a862
柱本珠希 <hashiramototamaki@bobcat-024-143.bates.edu>
2024-05-12 15:48:48 -0400

    created files for every HowTo and I'm making templates now

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import "./MainPage.css";
 import Navigation from "./navigation/Navigation";
-("");
+import HTMLHowTo from "./components/HTMLHowTo";
+import CSSHowTo from "./components/CSSHowTo";
+import GitHubHowTo from "./components/GitHubHowTo";
+import JavaScriptHowTo from "./components/JavaScriptHowTo";
+import ReactHowTo from "./components/ReactHowTo";
 
 function MainPage() {
   return (
@@ -9,7 +13,14 @@ function MainPage() {
       <div className="navigation">
         <Navigation />
       </div>
-      <div className="explanation">Hello</div>
+      <div className="explanation">
+        <HTMLHowTo />
+        <CSSHowTo />
+        <GitHubHowTo />
+        <JavaScriptHowTo />
+        <ReactHowTo />
+        Hello
+      </div>
     </div>
   );
 }
---
+++ src/components/HTMLHowTo.tsx
@@ -0,0 +1,12 @@
+import "../styles/HTMLHowTo.css";
+
+const HTMLHowTo = () => {
+  return (
+    <div className="html-how-to-container">
+      <h1>How to Use HTML</h1>
+      <p>This is a guide on how to use HTML...</p>
+    </div>
+  );
+};
+
+export default HTMLHowTo;
