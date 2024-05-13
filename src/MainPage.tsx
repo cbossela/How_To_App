bf6cfa52e855ad1a4f0ff60eb2e51fa0e1f54862
Jangmin Song <jsong3@bates.edu>
2024-05-12 22:01:08 -0400

    UX fixed

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,24 +1,46 @@
-import React from "react";
 import "./MainPage.css";
-import Navigation from "./navigation/Navigation";
 import HTMLHowTo from "./components/HTMLHowTo";
-// import CSSHowTo from "./components/CSSHowTo";
-// import GitHubHowTo from "./components/GitHubHowTo";
-// import JavaScriptHowTo from "./components/JavaScriptHowTo";
-// import ReactHowTo from "./components/ReactHowTo";
+import CSSHowTo from "./components/CSSHowTo";
+import GitHubHowTo from "./components/GitHubHowTo";
+import JavaScriptHowTo from "./components/JavaScriptHowTo";
+import ReactHowTo from "./components/ReactHowTo";
+import { useRef } from "react";
+import Navigation from "./navigation/Navigation";
 
 function MainPage() {
+  const HTMLSection = useRef<null | HTMLDivElement>(null);
+  const CSSSection = useRef<null | HTMLDivElement>(null);
+  const GitHubSection = useRef<null | HTMLDivElement>(null);
+  const JavaScriptSection = useRef<null | HTMLDivElement>(null);
+  const ReactSection = useRef<null | HTMLDivElement>(null);
+
   return (
     <div className="mainpage">
       <div className="navigation">
-        <Navigation />
+        <Navigation
+          HTMLSection={HTMLSection}
+          CSSSection={CSSSection}
+          GitHubSection={GitHubSection}
+          JavaScriptSection={JavaScriptSection}
+          ReactSection={ReactSection}
+        />
       </div>
       <div className="explanation">
-        <HTMLHowTo />
-        {/* <CSSHowTo />
-        <GitHubHowTo />
-        <JavaScriptHowTo />
-        <ReactHowTo /> */}
+        <div ref={HTMLSection}>
+          <HTMLHowTo />
+        </div>
+        {/* <div ref={CSSSection}>
+          <CSSHowTo />
+        </div> */}
+        <div ref={GitHubSection}>
+          <GitHubHowTo />
+        </div>
+        {/* <div ref={JavaScriptSection}>
+          <JavaScriptHowTo />
+        </div>
+        <div ref={ReactSection}>
+          <ReactHowTo />
+        </div> */}
       </div>
     </div>
   );
